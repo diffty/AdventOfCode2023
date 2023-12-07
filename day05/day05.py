5d230eb41781a569ab5f5917c4790627b6781620
DiFFtY <diffty@gmail.com>
2023-12-07 23:32:21 +0100

    day05 WIP

---
+++ day05/day05.py
@@ -0,0 +1,44 @@
+import re
+
+
+test_data = '''
+seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4
+'''
+
+data = test_data.strip()
+
+d = re.compile(r"([a-z\-]*) map:((?:[ \n\t\r]*\d+)*)", re.I)
+for it in d.finditer(data):
+    print(it.groups())
