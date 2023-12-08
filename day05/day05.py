823fb7e0d85fcda10354705602440bd29f793011
DiFFtY <diffty@gmail.com>
2023-12-08 05:13:12 +0100

    day05 P1 done

--- day05/day05.py
+++ day05/day05.py
@@ -1,4 +1,5 @@
 import re
+import os
 
 
 test_data = '''
@@ -38,7 +39,55 @@ humidity-to-location map:
 '''
 
 data = test_data.strip()
+data = open(os.path.dirname(__file__) + "/data.txt").read().strip()
 
-d = re.compile(r"([a-z\-]*) map:((?:[ \n\t\r]*\d+)*)", re.I)
+d = re.compile(r"([a-z\-]*)(?: ([a-z]+))?:((?:[ \n\t\r]*\d+)*)", re.I)
+d2 = re.compile(r"([0-9a-z]+)(?:-to-([0-9a-z]+))?", re.I)
+
+parsed_data = {}
+
 for it in d.finditer(data):
-    print(it.groups())
+    var_name, var_type, var_value = it.groups()
+    parsed_data[var_name] = (var_type, var_value.strip())
+
+seeds = list(map(int, parsed_data["seeds"][1].split()))
+
+def remap(value, src_start, src_end, dst_start, dst_end):
+    return dst_start + ((value - src_start) / (src_end - src_start)) * (dst_end - dst_start)
+
+mappings = {}
+mappers = {}
+
+for var_name, var_content in parsed_data.items():
+    var_type, var_value = var_content
+    if var_type == "map":
+        src, dst = d2.search(var_name).groups()
+        
+        mappings[src] = (dst, list(map(str.split, var_value.split("\n"))))
+        
+
+def make_mapping(dest_range_start, source_range_start, range_length):
+    return dict(zip(list(range(dest_range_start, dest_range_start + range_length)),
+                    list(range(source_range_start, source_range_start + range_length))))
+    
+
+def m(start_name, value):
+    curr_mapping = mappings[start_name]
+
+    while curr_mapping is not None:
+        for dst_range_start, src_range_start, range_length in curr_mapping[1]:
+            dst_range_start = int(dst_range_start)
+            src_range_start = int(src_range_start)
+            range_length = int(range_length)
+
+            if src_range_start <= value and value <= src_range_start + range_length:
+                value = dst_range_start + (value - src_range_start)
+                break
+
+        start_name = mappings[start_name][0]
+        curr_mapping = mappings.get(start_name, None)
+    
+    return value
+
+
+print(min([m("seed", s) for s in seeds]))
