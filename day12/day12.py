b6a982652cad967c253899b3cd8e34604578859e
DiFFtY <diffty@gmail.com>
2023-12-12 18:59:42 +0100

    day12: some extra info

--- day12/day12.py
+++ day12/day12.py
@@ -14,31 +14,26 @@ test_data = '''
 '''
 
 data = test_data
-data = open(os.path.dirname(__file__) + "/data.txt").read()
+#data = open(os.path.dirname(__file__) + "/data.txt").read()
 
 data_lines = data.strip().split('\n')
 
-total_arrangements = 0
+total_nb_arrangements = 0
 
-for l in data_lines:
-    row_map, broken_count_raw = l.split(" ")
+for line_num, line_data in enumerate(data_lines):
+    curr_line_nb_arrangements = 0
+
+    row_map, broken_count_raw = line_data.split(" ")
+    print(f"-- {line_num+1}/{len(data_lines)} ({round(line_num/len(data_lines)*100)}%) --")
+    print(row_map, broken_count_raw)
+
     broken_count = list(map(int, broken_count_raw.split(',')))
-
-    print(row_map, broken_count)
-
     unknown_coord_list = [res.start() for res in re.finditer('\?', row_map, re.I)]
-    print(unknown_coord_list)
-    
     nb_spaces = len(row_map) - sum(broken_count)
-    print(nb_spaces)
 
-    #spaces_combinations = [["".join(["."]*s) for s in range(0 if i in [0, len(broken_count)] else 1, nb_spaces)] for i in range(len(broken_count)+1)]
-    
     spaces_possibilities = [["" if i in [0, len(broken_count)] else None] + ["".join(["."]*s) for s in range(1, nb_spaces)] for i in range(len(broken_count)+1)]
-    print(spaces_possibilities)
 
     combinations = []
-
     w = len(spaces_possibilities[0])
     h = len(spaces_possibilities)
 
@@ -47,19 +42,21 @@ for l in data_lines:
         combinations.append([spaces_possibilities[n][c] for n, c in enumerate(comb)])
     
     combinations = list(filter(lambda c: None not in c and len("".join(c)) == nb_spaces, combinations))
-    print(combinations)
-    print(len(combinations))
 
     for spaces in combinations:
         result = ""
 
         for i, s in enumerate(spaces[:-1]):
-            result += s + "#"*broken_count[i]
+            result += s + "#" * broken_count[i]
 
         result += spaces[-1]
 
         if all([result[res.start()] == res.group(1) for res in re.finditer('([#.])', row_map, re.I)]):
             print(result)
-            total_arrangements += 1
+            curr_line_nb_arrangements += 1
+    
+    total_nb_arrangements += curr_line_nb_arrangements
+    print(f"{curr_line_nb_arrangements=}")
+    print()
 
-print(total_arrangements)
+print(f"{total_nb_arrangements=}")
