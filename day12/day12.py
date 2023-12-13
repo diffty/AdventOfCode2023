941d664e2fa3dbec620acbb17e2b851f98582905
DiFFtY <diffty@gmail.com>
2023-12-13 01:15:50 +0100

    day12 part1 done. super unoptimized slow bruteforce solution tho

--- day12/day12.py
+++ day12/day12.py
@@ -4,6 +4,9 @@ import math
 import itertools
 
 
+FP = open("output.log", "w")
+
+
 test_data = '''
 ???.### 1,1,3
 .??..??...?##. 1,1,3
@@ -13,50 +16,132 @@ test_data = '''
 ?###???????? 3,2,1
 '''
 
+test_data2 = '''
+????.#...#... 4,1,1
+#####.#### 5,4
+'''
+
+test_data3 = '''
+??????????????? 6,2
+'''
+
+
+def print_to_disk(*s):
+    s = list(map(str, s))
+    FP.write(" ".join(s))
+    FP.write("\n")
+    print(*s)
+
+
 data = test_data
-#data = open(os.path.dirname(__file__) + "/data.txt").read()
+data = open(os.path.dirname(__file__) + "/data.txt").read()
 
 data_lines = data.strip().split('\n')
+
+FP.close()
 
 total_nb_arrangements = 0
 
 for line_num, line_data in enumerate(data_lines):
+    FP = open("output.log", "a")
+
     curr_line_nb_arrangements = 0
 
     row_map, broken_count_raw = line_data.split(" ")
-    print(f"-- {line_num+1}/{len(data_lines)} ({round(line_num/len(data_lines)*100)}%) --")
-    print(row_map, broken_count_raw)
+    print_to_disk(f"-- {line_num+1}/{len(data_lines)} ({round(line_num/len(data_lines)*100)}%) --")
 
     broken_count = list(map(int, broken_count_raw.split(',')))
+    #print(f"{broken_count=}")
+
     unknown_coord_list = [res.start() for res in re.finditer('\?', row_map, re.I)]
+    #print(f"{unknown_coord_list=}")
+
     nb_spaces = len(row_map) - sum(broken_count)
+    #print(f"{nb_spaces=}")
 
-    spaces_possibilities = [["" if i in [0, len(broken_count)] else None] + ["".join(["."]*s) for s in range(1, nb_spaces)] for i in range(len(broken_count)+1)]
+    same = False
 
-    combinations = []
-    w = len(spaces_possibilities[0])
-    h = len(spaces_possibilities)
+    parsed_broken_blocks = re.findall("(#+)", row_map, re.I)
 
-    for i in range(((w ** h))):
-        comb = [int((i / (w**row if row > 0 else 1)) % w) for row in range(h)]
-        combinations.append([spaces_possibilities[n][c] for n, c in enumerate(comb)])
-    
-    combinations = list(filter(lambda c: None not in c and len("".join(c)) == nb_spaces, combinations))
+    if len(parsed_broken_blocks) == len(broken_count) and len(unknown_coord_list) == 0:
+        for broken_parts_block_num, res in enumerate(parsed_broken_blocks):
+            #print(broken_parts_block_num, res)
 
-    for spaces in combinations:
-        result = ""
+            if broken_parts_block_num >= len(broken_count):
+                break
 
-        for i, s in enumerate(spaces[:-1]):
-            result += s + "#" * broken_count[i]
+            if broken_count[broken_parts_block_num] != len(res):
+                break            
+        else:
+            print_to_disk(row_map)
+            print_to_disk("OMG???")
+            same = True
 
-        result += spaces[-1]
+    if same:
+        curr_line_nb_arrangements += 1
+    else:
+        spaces_permutations = []
 
-        if all([result[res.start()] == res.group(1) for res in re.finditer('([#.])', row_map, re.I)]):
-            print(result)
-            curr_line_nb_arrangements += 1
-    
+        for broken_count_idx in range(len(broken_count)+1):
+            single_permutation = []
+
+            if broken_count_idx in [0, len(broken_count)]:
+                single_permutation.append("")
+            else:
+                single_permutation.append(None)
+
+            for s in range(1, nb_spaces+1):
+                single_permutation.append("".join(["."] * s))
+            
+            spaces_permutations.append(single_permutation)
+
+        #print(f"{spaces_permutations=}")
+
+        w = len(spaces_permutations[0])
+        h = len(spaces_permutations)
+
+        print_to_disk(f"{w}^{h} = {w ** h}")
+        print_to_disk(row_map, broken_count_raw)
+
+        for i in range(w ** h):
+            comb = [int((i / (w**row if row > 0 else 1)) % w) for row in range(h)]
+            #print(comb)
+            curr_combination = [spaces_permutations[n][c] for n, c in enumerate(comb)]
+            #print(curr_combination)
+            #print_to_disk(curr_combination)
+
+            # if None not in curr_combination:
+            #     result = ""
+
+            #     for j, s in enumerate(curr_combination[:-1]):
+            #         result += s + "#" * broken_count[j]
+
+            #     result += curr_combination[-1]
+            #     print(
+            #         "✅" if len("".join(curr_combination)) == nb_spaces else "❌",
+            #         "✅" if all([result[res.start()] == res.group(1) for res in re.finditer('([#.])', row_map, re.I)]) else "❌",
+            #         result)
+            
+            if None not in curr_combination and len("".join(curr_combination)) == nb_spaces:
+                result = ""
+
+                for j, s in enumerate(curr_combination[:-1]):
+                   result += s + "#" * broken_count[j]
+
+                result += curr_combination[-1]
+
+                if all([result[res.start()] == res.group(1) for res in re.finditer('([#.])', row_map, re.I)]):
+                    print_to_disk(result)
+                    curr_line_nb_arrangements += 1
+        
     total_nb_arrangements += curr_line_nb_arrangements
-    print(f"{curr_line_nb_arrangements=}")
-    print()
+    print_to_disk(f"{curr_line_nb_arrangements=}")
+    print_to_disk()
 
-print(f"{total_nb_arrangements=}")
+    FP.close()
+
+FP = open("output.log", "a")
+
+print_to_disk(f"{total_nb_arrangements=}")
+
+FP.close()
