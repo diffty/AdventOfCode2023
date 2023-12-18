dd955c083f559c462dcafcb7eea3abb6d601e189
DiFFtY <diffty@gmail.com>
2023-12-18 10:18:27 +0100

    day14 some cleaning to prepare part 2

--- day14/day14.py
+++ day14/day14.py
@@ -18,28 +18,26 @@ data = open(os.path.dirname(__file__) + "/data.txt").read()
 
 data_lines = data.strip().split()
 
-data_lines_rot = list(map(lambda l: "".join(l), zip(*data_lines)))
-print(data_lines_rot)
 
-new_data_lines_rot = []
+def make_boulders_roll(data_lines):
+    data_lines_rot = list(map(lambda l: "".join(l), zip(*data_lines)))
 
-weight = 0
+    weight = 0
 
-for i, l in enumerate(data_lines_rot):
-    splitted_line = l.split('#')
-    pos = len(l)
+    for i, l in enumerate(data_lines_rot):
+        splitted_line = l.split('#')
+        pos = len(l)
 
-    for j, section in enumerate(splitted_line):
-        nb_boulders = section.count("O")
-        splitted_line[j] = ("O" * nb_boulders).ljust(len(section), '.')
-        print(list(range(pos-nb_boulders+1,pos+1)))
-        weight += sum(range(pos-nb_boulders+1,pos+1))
-        pos -= len(section)+1
-        print(weight)
-    
-    data_lines_rot[i]  = "#".join(splitted_line)
+        for j, section in enumerate(splitted_line):
+            nb_boulders = section.count("O")
+            splitted_line[j] = ("O" * nb_boulders).ljust(len(section), '.')
+            weight += sum(range(pos-nb_boulders+1,pos+1))
+            pos -= len(section)+1
+        
+        data_lines_rot[i]  = "#".join(splitted_line)
 
-data_lines = list(map(lambda l: "".join(l), zip(*data_lines_rot)))
-print(data_lines)
+    data_lines = list(map(lambda l: "".join(l), zip(*data_lines_rot)))
 
-print(weight)
+    return data_lines, weight
+
+print(make_boulders_roll(data_lines))
